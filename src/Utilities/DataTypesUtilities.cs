d4f25d2d36ebb646a667d35b3afd1825acfd4fcf
JakubMiodunka <miodunkajakub@gmail.com>
2024-04-06 15:40:45 +0200

    Transferring locally stored project to GIT repository.

---
+++ src/Utilities/DataTypesUtilities.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+
+namespace Utilities
+{
+    /// <summary>
+    /// Set of static methods helpful during interactions with C# data types.
+    /// </summary>
+    public static class DataTypesUtilities
+    {
+        /// <summary>
+        /// Determines maximal value, that can be stored by specified numeric data type.
+        /// </summary>
+        /// <param name="type">
+        /// Numeric data type, which maximal value shall be determined.
+        /// </param>
+        /// <returns>
+        /// Maximal value, which provided numeric data type can store.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown, when specified data type is a null reference.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown, when specified data type is not sported.
+        /// </exception>
+        public static double GetTypeMaxValue(Type type)
+        {
+            if (type is null)
+            {
+                const string ErrorMessage = "Specified data type is a null reference:";
+                throw new ArgumentNullException(ErrorMessage);
+            }
+
+            TypeCode typeCode = Type.GetTypeCode(type);
+
+            switch (typeCode)
+            {
+                case TypeCode.Byte:
+                    return Convert.ToDouble(Byte.MaxValue);
+                case TypeCode.SByte:
+                    return Convert.ToDouble(SByte.MaxValue);
+                case TypeCode.UInt16:
+                    return Convert.ToDouble(UInt16.MaxValue);
+                case TypeCode.UInt32:
+                    return Convert.ToDouble(UInt32.MaxValue);
+                case TypeCode.UInt64:
+                    return Convert.ToDouble(UInt64.MaxValue);
+                case TypeCode.Int16:
+                    return Convert.ToDouble(Int16.MaxValue);
+                case TypeCode.Int32:
+                    return Convert.ToDouble(Int32.MaxValue);
+                case TypeCode.Int64:
+                    return Convert.ToDouble(Int64.MaxValue);
+                case TypeCode.Decimal:
+                    return Convert.ToDouble(Decimal.MaxValue);
+                case TypeCode.Double:
+                    return Convert.ToDouble(Double.MaxValue);
+                case TypeCode.Single:
+                    return Convert.ToDouble(Single.MaxValue);
+                default:
+                    string errorMessage = $"Specified data type is not supported: {type.Name}";
+                    throw new ArgumentOutOfRangeException(errorMessage);
+            }
+        }
+
+        /// <summary>
+        /// Determines minimal value, that can be stored by specified numeric data type.
+        /// </summary>
+        /// <param name="type">
+        /// Numeric data type, which minimal value shall be determined.
+        /// </param>
+        /// <returns>
+        /// Minimal value, which provided numeric data type can store.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown, when specified data type is a null reference.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown, when specified data type is not sported.
+        /// </exception>
+        public static double GetTypeMinValue(Type type)
+        {
+            if (type is null)
+            {
+                const string ErrorMessage = "Specified data type is a null reference:";
+                throw new ArgumentNullException(ErrorMessage);
+            }
+
+            TypeCode typeCode = Type.GetTypeCode(type);
+
+            switch (typeCode)
+            {
+                case TypeCode.Byte:
+                    return Convert.ToDouble(Byte.MinValue);
+                case TypeCode.SByte:
+                    return Convert.ToDouble(SByte.MinValue);
+                case TypeCode.UInt16:
+                    return Convert.ToDouble(UInt16.MinValue);
+                case TypeCode.UInt32:
+                    return Convert.ToDouble(UInt32.MinValue);
+                case TypeCode.UInt64:
+                    return Convert.ToDouble(UInt64.MinValue);
+                case TypeCode.Int16:
+                    return Convert.ToDouble(Int16.MinValue);
+                case TypeCode.Int32:
+                    return Convert.ToDouble(Int32.MinValue);
+                case TypeCode.Int64:
+                    return Convert.ToDouble(Int64.MinValue);
+                case TypeCode.Decimal:
+                    return Convert.ToDouble(Decimal.MinValue);
+                case TypeCode.Double:
+                    return Convert.ToDouble(Double.MinValue);
+                case TypeCode.Single:
+                    return Convert.ToDouble(Single.MinValue);
+                default:
+                    string errorMessage = $"Specified data type is not supported: {type.Name}";
+                    throw new ArgumentOutOfRangeException(errorMessage);
+            }
+        }
+
+    }
+}
---
+++ src/Utilities/FileSystemUtilities.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+
+namespace Utilities
+{
+    /// <summary>
+    /// Set of static methods helpful during interactions with a file system.
+    /// </summary>
+    public static class FileSystemUtilities
+    {
+        /// <summary>
+        /// Checks if given file system entry exists and is a directory.
+        /// If either of above cases would not be fulfilled, according exception will be raised.
+        /// </summary>
+        /// <param name="path">
+        /// Path to file system entry, that is required to be validated.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown, when given path is null reference.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown, when given path is an empty string.
+        /// </exception>
+        /// <exception cref="DirectoryNotFoundException">
+        /// Thrown, when provided file system entry does not exist in file system as directory.
+        /// </exception>
+        public static void ValidateExistingDirectory(string path)
+        {
+            if (path is null)
+            {
+                throw new ArgumentNullException("Entry path is a null reference:");
+            }
+
+            if (path == string.Empty)
+            {
+                throw new ArgumentOutOfRangeException("Entry path is an empty string:");
+            }
+
+            if (!Directory.Exists(path))
+            {
+                if (File.Exists(path))
+                {
+                    throw new DirectoryNotFoundException($"Given file system entry is a file: {path}");
+                }
+
+                throw new DirectoryNotFoundException($"Directory does not exist: {path}");
+            }
+        }
+
+        /// <summary>
+        /// Checks if given file system entry does not exists as directory in file system.
+        /// If validation fails, according exception will be thrown.
+        /// </summary>
+        /// <param name="path">
+        /// Path to file system entry, that is required to be validated.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown, when given path is null reference.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown, when given path is an empty string.
+        /// </exception>
+        /// <exception cref="IOException">
+        /// Thrown, when provided file system entry exists in file system as directory.
+        /// </exception>
+        public static void ValidateNonExistingDirectory(string path)
+        {
+            if (path is null)
+            {
+                throw new ArgumentNullException("Entry path is a null reference:");
+            }
+
+            if (path == string.Empty)
+            {
+                throw new ArgumentOutOfRangeException("Entry path is an empty string:");
+            }
+
+            if (Directory.Exists(path))
+            {
+                throw new IOException($"Directory already exists: {path}");
+            }
+        }
+
+        /// <summary>
+        /// Checks if given file system entry has valid extension.
+        /// If validation fails, according exception will be thrown.
+        /// </summary>
+        /// <param name="path">
+        /// Path, that is required to be validated.
+        /// </param>
+        /// <param name="extensions">
+        /// File extensions, that shall be considered as valid during the process.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown, when given path or at least one of given extensions is null reference.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown, when given path or at least one of given extension is an empty string.
+        /// </exception>
+        /// <exception cref="IOException">
+        /// Thrown, when validation fails.
+        /// </exception>
+        public static void ValidateExtension(string path, params string[] extensions)
+        {
+            if (path is null)
+            {
+                throw new ArgumentNullException("Entry path is a null reference:");
+            }
+
+            if (path == string.Empty)
+            {
+                throw new ArgumentOutOfRangeException("Entry path is an empty string:");
+            }
+
+            if (extensions is null)
+            {
+                throw new ArgumentNullException("Extensions array is a null reference:");
+            }
+
+            if (extensions.Contains(null))
+            {
+                throw new ArgumentNullException("One of given extensions is a null reference:");
+            }
+
+            if (extensions.Contains(string.Empty))
+            {
+                throw new ArgumentNullException("One of given extensions is an empty string:");
+            }
+
+
+            if (extensions.Any())
+            {
+                string extension = Path.GetExtension(path);
+
+                if (!extensions.Contains(extension))
+                {
+                    throw new IOException($"Invalid extension: is '{extension}', but shall be one of '{string.Join(", ", extensions)}'");
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Checks if given file system entry exists, is a file and its extension is among given ones.
+        /// If either of above cases would not be fulfilled, according exception will be thrown.
+        /// </summary>
+        /// <param name="path">
+        /// Path to file system entry, that is required to be validated.
+        /// </param>
+        /// <param name="extensions">
+        /// File extensions, that shall be considered as valid during the process.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown, when given path or one of file extensions is null reference.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown, when given path is an empty string.
+        /// </exception>
+        /// <exception cref="FileNotFoundException">
+        /// Thrown, when given file system entry is not a file or does not exists.
+        /// </exception>
+        /// <exception cref="IOException">
+        /// Thrown, when provided file system entry has invalid extension.
+        /// </exception>
+        public static void ValidateExistingFile(string path, params string[] extensions)
+        {
+            if (path is null)
+            {
+                throw new ArgumentNullException("Entry path is a null reference:");
+            }
+
+            if (path == string.Empty)
+            {
+                throw new ArgumentOutOfRangeException("Entry path is an empty string:");
+            }
+
+            if (!File.Exists(path))
+            {
+                if (Directory.Exists(path))
+                {
+                    throw new FileNotFoundException($"Given file system entry is a directory: {path}");
+                }
+
+                throw new FileNotFoundException($"File does not exist: {path}");
+            }
+
+            ValidateExtension(path, extensions);
+        }
+
+        /// <summary>
+        /// Checks if given file system entry does not exists as a file in file system and its extension is among given ones.
+        /// If either of above cases would not be fulfilled, according exception will be thrown.
+        /// </summary>
+        /// <param name="path">
+        /// Path to file system entry, that is required to be validated.
+        /// </param>
+        /// <param name="extensions">
+        /// File extensions, that shall be considered as valid during the process.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown, when given path or one of file extensions is null reference.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown, when given path is an empty string.
+        /// </exception>
+        /// <exception cref="IOException">
+        /// Thrown, when provided file system entry has invalid extension or when it exists in file system as file.
+        /// </exception>
+        public static void ValidateNonExistingFile(string path, params string[] extensions)
+        {
+            if (path is null)
+            {
+                throw new ArgumentNullException("Entry path is a null reference:");
+            }
+
+            if (path == string.Empty)
+            {
+                throw new ArgumentOutOfRangeException("Entry path is an empty string:");
+            }
+
+            if (File.Exists(path))
+            {
+                throw new IOException($"Given file already exists: {path}");
+            }
+
+            ValidateExtension(path, extensions);
+        }
+    }
+}
